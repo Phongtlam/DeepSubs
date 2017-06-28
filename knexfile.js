8cdc98dd4abac444fffba0354e77cf8bf1ba6b00
Phongtlam <Phongtlam@gmail.com>
2017-06-28 00:00:52 -0700

    heroku

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,15 @@
 // Update with your config settings.
+require('dotenv').config('./.env');
+
 
 module.exports = {
   client: 'postgresql',
   connection: {
-    database: 'deep_subs',
-    user: 'deeper',
-    password: 'valky1223',
-    host: 'localhost',
-    port: 5432,
+    database: process.env.DATABASE_URL,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
   },
   migrations: {
     tableName: 'knex_migrations',
